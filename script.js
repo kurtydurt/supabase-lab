523dcf45f689102fafaee8805a6504e0d2c0c195
Sam Kurtzman <kurtzma4@msu.edu>
2024-03-18 10:31:19 -0400

    Script type module fix for CDN

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.7.1"
 const supabaseUrl = 'https://ditdxrktdzgcigyoazjf.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpdGR4cmt0ZHpnY2lneW9hempmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzNDExMTAsImV4cCI6MjAyNTkxNzExMH0.EuWZ3jqO_zjuURqQG7EUFfq1ix3Svw8xeRikamtUZXg'
 const supabase = createClient(supabaseUrl, supabaseKey)
